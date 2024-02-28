ee822f722b29d6b49cf8c00c3a4f9289d64bb2af
Nio Homy <NIOHOMY@mail.ru>
2024-02-28 20:18:35 +0300

    two triangles

--- openGL.cpp
+++ openGL.cpp
@@ -18,6 +18,205 @@
 
 const char* vertexShaderSource = R"(
 #version 330 core
+layout (location = 0) in vec3 aPos;
+
+layout (location = 1) in vec3 aColor;
+out vec3 color;
+
+uniform mat4 rotation;
+void main()
+{
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	color = aColor;
+}
+)";
+
+const char* fragmentShader1Source = R"(
+#version 330 core
+in vec3 color;
+out vec4 FragColor;
+
+
+void main()
+{
+	FragColor = vec4(0.4f, 0.1f, 0.5f, 1.0f);
+	FragColor = vec4(color, 1.0f);
+}
+)";
+
+const char* fragmentShader2Source = R"(
+#version 330 core
+in vec3 color;
+out vec4 FragColor;
+void main()
+{
+	FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+	FragColor = vec4(color, 1.0f);
+}
+)";
+
+int main()
+{
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return 1;
+	}
+
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+	if (!window) {
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	const GLubyte* renderer = glGetString(GL_RENDERER); 
+	const GLubyte* version = glGetString(GL_VERSION); 
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	/* tell GL to only draw onto a pixel if the shape is closer to the viewer
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+	/* OTHER STUFF GOES HERE NEXT */
+
+	// close GL context and any other GLFW resources
+	//glfwTerminate();
+
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	unsigned int fragmenShaderFirst = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmenShaderSecond = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	unsigned int shaderProgramFirst = glCreateProgram();
+	unsigned int shaderProgramSecond = glCreateProgram();
+	
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glShaderSource(fragmenShaderFirst, 1, &fragmentShader1Source, NULL);
+	glCompileShader(fragmenShaderFirst);
+
+	glShaderSource(fragmenShaderSecond, 1, &fragmentShader2Source, NULL);
+	glCompileShader(fragmenShaderSecond);
+
+	
+	glAttachShader(shaderProgramFirst, vertexShader);
+	glAttachShader(shaderProgramFirst, fragmenShaderFirst);
+	glLinkProgram(shaderProgramFirst);
+
+	
+	glAttachShader(shaderProgramSecond, vertexShader);
+	glAttachShader(shaderProgramSecond, fragmenShaderSecond);
+	glLinkProgram(shaderProgramSecond);
+
+	float firstTriangle[] = {
+		
+		-0.9f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,   
+		-0.0f, -0.5f, 0.0f,  0.0f, 1.0f, 1.0f,   
+		-0.45f, 0.5f, 0.0f,  0.0f, 0.0f, 0.0f    
+	};
+	float secondTriangle[] = {
+		
+		0.0f, -0.5f, 0.0f,  1.0f, 0.0f, 0.2f,    
+		0.9f, -0.5f, 0.0f,  0.2f, 0.0f, 1.0f,    
+		0.45f, 0.5f, 0.0f,  1.0f, 0.0f, 1.0f     
+	};
+
+
+	/*
+	float firstTriangle[] = {
+		-0.9f, -0.5f,  0.0f,
+		-0.0f, -0.5f,  0.0f,
+		-0.45f, 0.5f,  0.0f
+	};
+	float secondTriangle[] = {
+		0.0f, -0.5f,  0.0f,
+		0.9f, -0.5f,  0.0f,
+		0.45f, 0.5f,  0.0f
+	};
+	*/
+
+	unsigned int VBOs[2], VAOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+
+	// 1
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // Цвета
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);// Цвета
+
+	// 2
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // Цвета
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);// Цвета
+
+	while (!glfwWindowShouldClose(window)) {
+		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		
+		glUseProgram(shaderProgramFirst);
+		glBindVertexArray(VAOs[0]);
+
+		/*
+		glm::mat4  model(1.0f);
+		//model = glm::translate(model, glm::vec3(0.3f, -0.3f, 0.0f));
+		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+		GLuint rotationLoc = glGetUniformLocation(shaderProgramFirst, "rotation");
+		glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, &model[0][0]);
+		*/
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+		glUseProgram(shaderProgramSecond);
+		glBindVertexArray(VAOs[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		
+	}
+
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
+
+
+//while (!glfwWindowShouldClose(window)) {
+	//    // очищаем экран
+	//    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//    // обновить другие события, такие как обработка ввода
+	//    glfwPollEvents();
+	//    // поместите то, что мы рисовали, на дисплей
+	//    glfwSwapBuffers(window);
+	//}
+/*
+const char* vertexShaderSource = R"(
+#version 330 core
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
@@ -33,140 +232,4 @@ void main()
 }
 
 )";
-
-const char* fragmentShaderSource = R"(
-#version 330 core
-
-in vec3 color;
-
-out vec4 fragColor;
-
-void main()
-{
-    fragColor = vec4(color, 1.0);
-}
-)";
-
-int main()
-{
-    // Инициализируем GLFW
-    if (!glfwInit()) {
-        fprintf(stderr, "ERROR: could not start GLFW3\n");
-        return 1;
-    }
-
-    // Создаем окно
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
-    if (!window) {
-        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-        glfwTerminate();
-        return 1;
-    }
-
-    // Создаем контекст OpenGL и привязываем его к окну
-    glfwMakeContextCurrent(window);
-    glEnable(GL_DEPTH_TEST);
-    
-    // Инициализируем GLEW
-    GLenum err = glewInit();
-    if (GLEW_OK != err) {
-        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-        glfwTerminate();
-        return 1;
-    }
-
-
-    // Массив вершин для треугольника
-    GLfloat vertices[] = {
-       0.0f,  0.5f, 0.0f, // Вершина 
-       0.8f, 0.2f, 0.8f, // Цвет голуб
-      -0.5f, -0.5f, 0.0f, // Вершина 
-       0.2f, 0.2f, 0.2f, // Цвет
-       0.5f, -0.5f, 0.0f, // Вершина 
-       0.2f, 0.2f, 0.6f  // Цвет 
-    };
-
-    
-
-
-    // Создаем буфер вершин и связываем его
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Создаем шейдерную программу
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-
-    // Связываем атрибуты вершин с буфером вершин
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "aPos");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
-
-    GLint colAttrib = glGetAttribLocation(shaderProgram, "aColor");
-    glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-
-    
-
-    // Основной цикл приложения
-    while (!glfwWindowShouldClose(window)) {
-        // Очищаем экран
-        glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        
-        glm::mat4  model(1.0f);
-        //glm::mat4 rotation = glm::rotate(glm::radians(30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
-        model = glm::translate(model, glm::vec3(0.3f, -0.3f, 0.0f));
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        GLuint rotationLoc = glGetUniformLocation(shaderProgram, "rotation");
-        glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, &model[0][0]);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        // Обновляем другие события, такие как обработка ввода
-        glfwPollEvents();
-
-        // Помещаем то, что мы нарисовали, на дисплей
-        glfwSwapBuffers(window);
-    }
-
-    // Удаляем буфер вершин и шейдерную программу
-    glDeleteBuffers(1, &vertexBuffer);
-    glDeleteProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Закрываем окно и завершаем работу GLFW
-    glfwTerminate();
-
-    return 0;
-}
-
-
-
-//while (!glfwWindowShouldClose(window)) {
-    //    // очищаем экран
-    //    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-    //    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //    // обновить другие события, такие как обработка ввода
-    //    glfwPollEvents();
-    //    // поместите то, что мы рисовали, на дисплей
-    //    glfwSwapBuffers(window);
-    //}
+*/
