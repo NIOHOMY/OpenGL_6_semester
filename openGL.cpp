4c1bcfceb9c1b3c90f13527634ca6dc988ad4a26
Nio Homy <NIOHOMY@mail.ru>
2024-03-08 14:31:05 +0300

    wind spinner  model with rainbow texture

--- openGL.cpp
+++ openGL.cpp
@@ -1,23 +1,70 @@
-﻿#include <GL/glew.h>
+﻿
+#define GLM_ENABLE_EXPERIMENTAL
+#include <GL/glew.h>
 #include<GL/glut.h>
 #include <GLFW/glfw3.h> 
 #include <stdio.h>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<glm/gtx/transform.hpp>
+
+
 #include <soil.h>
 #include "shader_s.h"
 //----------------------------------------------------------------
+//float vertices[] = {
+//	0.5f,  0.5f,  0.0f,		1.0f,0.0f, 0.0f,		 1.0f, 1.0f,
+//	0.5f, -0.5f,  0.0f,		0.0f, 1.0f, 0.0f,		 1.0f, 0.0f,
+//	-0.5f, -0.5f,  0.0f,	0.0f, 0.0f,  1.0f,		 0.0f, 0.0f,
+//	-0.5f, 0.5f,  0.0f,		1.0f, 1.0f,  0.0f,		 0.0f, 1.0f,
+//};
+//unsigned int indices[] =
+//{
+//	0,1,3,
+//	1,2,3
+//};
+
 float vertices[] = {
-	0.5f,  0.5f,  0.0f,		1.0f,0.0f, 0.0f,		 1.0f, 1.0f,
-	0.5f, -0.5f,  0.0f,		0.0f, 1.0f, 0.0f,		 1.0f, 0.0f,
-	-0.5f, -0.5f,  0.0f,	0.0f, 0.0f,  1.0f,		 0.0f, 0.0f,
-	-0.5f, 0.5f,  0.0f,		1.0f, 1.0f,  0.0f,		 0.0f, 1.0f,
+	0.0f,  0.0f,  0.0f,		1.0f,0.0f, 0.0f,		 1.0f, 1.0f,
+
+	0.0f,  7.0f,  0.0f,		1.0f,0.0f, 0.0f,		 0.5f, 1.0f,
+   -0.2f, 0.0f,  0.0f,		0.0f, 1.0f, 0.0f,		 1.0f, 0.5f,
+	0.2f, 0.0f,  0.0f,	    0.0f, 0.0f,  1.0f,		 0.5f, 0.5f,
+	
+	1.0f,  5.0f,  0.0f,		1.0f,0.0f, 0.0f,		 1.0f, 1.0f,
+   -1.0f, 4.0f,  0.0f,		0.0f, 1.0f, 0.0f,		 0.0f, 0.0f,
+
+   2.0f,  9.0f,  0.0f,		1.0f,0.0f, 0.0f,		 0.5f, 0.0f,
+   4.0f, 7.0f,  0.0f,		0.0f, 1.0f, 0.0f,		 0.0f, 0.0f,
+
+   -2.5f,  7.0f,  0.0f,		1.0f,0.0f, 0.0f,		 0.0f, 0.0f,
+   -2.5f, 9.0f,  0.0f,		0.0f, 1.0f, 0.0f,		 1.0f, 0.0f,
+
+   2.0f,  7.0f,  0.0f,		1.0f,0.0f, 0.0f,		 0.0f, 1.0f,
+   2.0f, 5.0f,  0.0f,		0.0f, 1.0f, 0.0f,		 0.0f, 0.0f,
+
+   -3.0f,  6.0f,  0.0f,		1.0f,0.0f, 0.0f,		 0.0f, 1.0f,
+   -0.5f, 5.5f,  0.0f,		0.0f, 1.0f, 0.0f,		 0.0f, 0.0f,
+
+   -1.5f,  8.0f,  0.0f,		1.0f,0.0f, 0.0f,		 1.0f, 1.0f,
+   -0.5f, 10.5f,  0.0f,		0.0f, 1.0f, 0.0f,		 1.0f, 0.0f,
 };
 unsigned int indices[] =
 {
-	0,1,3,
-	1,2,3
+	1,4,5,
+	1,6,7,
+	1,8,9,
+
+	7,10,11,
+	5,12,13,
+	9,14,15,
+
+	1, 2, 3
 };
+
 //----------------------------------------------------------------
 
 int main()
@@ -28,7 +75,7 @@ int main()
 		return 1;
 	}
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello texture", NULL, NULL);
 	if (!window) {
 		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
 		glfwTerminate();
@@ -80,7 +127,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nChannels;
-	unsigned char* data = SOIL_load_image("texture1.png", &width, &height, 0, SOIL_LOAD_RGB);
+	unsigned char* data = SOIL_load_image("rainbow_texture.png", &width, &height, 0, SOIL_LOAD_RGB);
 
 	if (data)
 	{
@@ -100,15 +147,28 @@ int main()
 	//----------------------------------------------------------------
 
 	while (!glfwWindowShouldClose(window)) {
-
+		glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+		glm::mat4 view = glm::lookAt(
+			glm::vec3(0.0f, 1.0f, -15.0f),
+			glm::vec3(0.0f, 5.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4  model(1.0f);
+
+		glm::mat4 mvp = projection * view * model;
+
+		GLuint mvpLoc = glGetUniformLocation(ourShader.ID, "mvp");
+		glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, &mvp[0][0]);
 
 		ourShader.use();
 		glBindVertexArray(VAO);
 		glEnable(GL_DEPTH_TEST);
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 21, GL_UNSIGNED_INT, 0);
+
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
--- vertexshader.vs
+++ vertexshader.vs
@@ -7,8 +7,10 @@ layout(location = 2) in vec2 aTexCoord;
 out vec3 ourColor;
 out vec2 ourTexCoord;
 
+uniform mat4 mvp;
+
 void main() {
-    gl_Position = vec4(aPos, 1.0); 
+    gl_Position =  mvp*vec4(aPos, 1.0); 
     ourColor = aColor;
     ourTexCoord = vec2(aTexCoord.x, aTexCoord.y); 
 
