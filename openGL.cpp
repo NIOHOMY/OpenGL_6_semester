420783b6d5a11c04caaa06074f55766c5ef04235
Nio Homy <NIOHOMY@mail.ru>
2024-03-31 14:30:33 +0300

    stand and new tex added

--- openGL.cpp
+++ openGL.cpp
@@ -28,8 +28,8 @@ const unsigned int SCR_HEIGHT = 400;
 
 // Камера
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
+float lastX = SCR_WIDTH / 1.4f;
+float lastY = SCR_HEIGHT / 1.4f;
 bool firstMouse = true;
 
 // Тайминги
@@ -37,7 +37,7 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // Освещение
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(1.2f, 1.0f, 1.4f);
 
 int main()
 {
@@ -81,53 +81,121 @@ int main()
 	Shader lampShader("lamp.vs", "lamp.fs");
 
 	// Указание вершин (и буфера(ов)) и настройка вершинных атрибутов
+	float secondObjectVertices[] = {
+		// Первая пирамида
+		// Основание 1
+		-1.4f, -1.25f, -1.4f,   1.0f, 1.0f,  0.0f,
+		 1.4f, -1.25f, -1.4f,   0.0f, 1.0f,  0.0f,
+		-1.4f, -1.25f,  1.4f,   1.0f, 0.0f,  0.0f,
+		-1.4f, -1.25f,  1.4f,   1.0f, 1.0f,  0.0f,
+		 1.4f, -1.25f,  1.4f,   1.0f, 0.0f,  0.0f,
+		 1.4f, -1.25f, -1.4f,   0.0f, 0.0f,  0.0f,
+		 // Основание 2
+		 -1.4f, -1.75f, -1.4f,  1.0f, 1.0f,  0.0f,
+		  1.4f, -1.75f, -1.4f,  0.0f, 1.0f,  0.0f,
+		 -1.4f, -1.75f,  1.4f,  1.0f, 0.0f,  0.0f,
+		 -1.4f, -1.75f,  1.4f,  1.0f, 1.0f,  0.0f,
+		  1.4f, -1.75f,  1.4f,  1.0f, 0.0f,  0.0f,
+		  1.4f, -1.75f, -1.4f,  0.0f, 0.0f,  0.0f,
+
+		 // бок1
+		-1.4f, -1.25f,  1.4f,  1.0f, 1.0f,  0.0f,
+		 1.4f, -1.25f,  1.4f,  0.0f, 1.0f,  0.0f,
+		-1.4f, -1.75f,  1.4f,  1.0f, 0.0f,  0.0f,
+		-1.4f, -1.75f,  1.4f,  1.0f, 1.0f,  0.0f,
+		 1.4f, -1.75f,  1.4f,  1.0f, 0.0f,  0.0f,
+		 1.4f, -1.25f,  1.4f,  0.0f, 0.0f,  0.0f,		 
+		 // бок2
+		-1.4f, -1.25f,  -1.4f, 1.0f, 1.0f,  0.0f,
+		 1.4f, -1.25f,  -1.4f, 0.0f, 1.0f,  0.0f,
+		-1.4f, -1.75f,  -1.4f, 1.0f, 0.0f,  0.0f,
+		-1.4f, -1.75f,  -1.4f, 1.0f, 1.0f,  0.0f,
+		 1.4f, -1.75f,  -1.4f, 1.0f, 0.0f,  0.0f,
+		 1.4f, -1.25f,  -1.4f, 0.0f, 0.0f,  0.0f,
+		 // бок3
+		 1.4f, -1.25f,  1.4f,   1.0f, 1.0f,  0.0f,
+		 1.4f, -1.25f, -1.4f,   0.0f, 1.0f,  0.0f,
+		 1.4f, -1.75f,  1.4f,   1.0f, 0.0f,  0.0f,
+		 1.4f, -1.75f,  1.4f,   1.0f, 1.0f,  0.0f,
+		 1.4f, -1.75f, -1.4f,   1.0f, 0.0f,  0.0f,
+		 1.4f, -1.25f, -1.4f,   0.0f, 0.0f,  0.0f,
+		 // бок4
+		 -1.4f, -1.25f,  1.4f,  1.0f, 1.0f,  0.0f,
+		 -1.4f, -1.25f, -1.4f,  0.0f, 1.0f,  0.0f,
+		 -1.4f, -1.75f,  1.4f,  1.0f, 0.0f,  0.0f,
+		 -1.4f, -1.75f,  1.4f,  1.0f, 1.0f,  0.0f,
+		 -1.4f, -1.75f, -1.4f,  1.0f, 0.0f,  0.0f,
+		 -1.4f, -1.25f, -1.4f,  0.0f, 0.0f,  0.0f,
+		 // пирамида
+		// Основание
+		-1.0f, -3.0f, -1.0f,   1.0f, 1.0f,  0.0f,
+		 1.0f, -3.0f, -1.0f,   0.0f, 1.0f,  0.0f,
+		 1.0f, -3.0f,  1.0f,   1.0f, 0.0f,  0.0f,
+		-1.0f, -3.0f, -1.0f,   1.0f, 1.0f,  0.0f,
+		 1.0f, -3.0f,  1.0f,   1.0f, 0.0f,  0.0f,
+		-1.0f, -3.0f,  1.0f,   0.0f, 0.0f,  0.0f,
+		// Боковые грани
+		-1.0f, -3.0f, -1.0f,  0.5f, 0.7071f, -1.7071f,
+		 1.0f, -3.0f, -1.0f,   1.5f, 0.7071f, -1.7071f,
+		 0.0f, -1.25f,  0.0f,   1.5f, 2.7071f, -1.7071f,
+		-1.0f, -3.0f,  1.0f,  1.5f, 2.7071f,  1.7071f,
+		 1.0f, -3.0f,  1.0f,   0.5f, 2.7071f,  1.7071f,
+		 0.0f, -1.25f,  0.0f,   0.5f, 0.7071f,  1.7071f,
+		-1.0f, -3.0f, -1.0f,  0.5f, 0.7071f, -1.7071f,
+		-1.0f, -3.0f,  1.0f,  1.5f, 0.7071f,  1.7071f,
+		 0.0f, -1.25f,  0.0f,  1.5f, 2.7071f, -1.7071f,
+		 1.0f, -3.0f, -1.0f,   1.5f, 2.7071f, -1.7071f,
+		 1.0f, -3.0f,  1.0f,   0.5f, 2.7071f,  1.7071f,
+		 0.0f, -1.25f,  0.0f,   0.5f, 0.7071f,  1.7071f,
+
+	};
 	float vertices[] = {
 		// Первая пирамида
 		// Основание
-		-0.5f, 0.0f, -0.5f,   0.5f, 1.5f,  0.5f,
-		 0.5f, 0.0f, -0.5f,   0.5f, 1.5f,  0.5f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 1.5f,  0.5f,
-		-0.5f, 0.0f, -0.5f,   0.5f, 1.5f,  0.5f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 1.5f,  0.5f,
-		-0.5f, 0.0f,  0.5f,   0.5f, 1.5f,  0.5f,
+		-0.5f, 0.5f, -0.5f,   0.5f, 1.5f,  0.5f,
+		 0.5f, 0.5f, -0.5f,   0.5f, 1.5f,  0.5f,
+		 0.5f, 0.5f,  0.5f,   0.5f, 1.5f,  0.5f,
+		-0.5f, 0.5f, -0.5f,   0.5f, 1.5f,  0.5f,
+		 0.5f, 0.5f,  0.5f,   0.5f, 1.5f,  0.5f,
+		-0.5f, 0.5f,  0.5f,   0.5f, 1.5f,  0.5f,
 		// Боковые грани
-		-0.5f, 0.0f, -0.5f,  -0.5f, 0.7071f, -1.7071f,
-		 0.5f, 0.0f, -0.5f,   1.5f, 0.7071f, -1.7071f,
-		 0.0f, 1.0f,  0.0f,   1.5f, 2.7071f, -1.7071f,
-		-0.5f, 0.0f,  0.5f,  -1.5f, 2.7071f,  1.7071f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 2.7071f,  1.7071f,
-		 0.0f, 1.0f,  0.0f,   0.5f, 0.7071f,  1.7071f,
-		-0.5f, 0.0f, -0.5f,  -0.5f, 0.7071f, -1.7071f,
-		-0.5f, 0.0f,  0.5f,  -1.5f, 0.7071f,  1.7071f,
-		 0.0f, 1.0f,  0.0f,  -1.5f, 2.7071f, -1.7071f,
-		 0.5f, 0.0f, -0.5f,   1.5f, 2.7071f, -1.7071f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 2.7071f,  1.7071f,
-		 0.0f, 1.0f,  0.0f,   0.5f, 0.7071f,  1.7071f,
+		-0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		 0.5f, 0.5f, -0.5f,  0.0f, 1.0f,  -1.7071f,
+		 0.0f, 1.5f,  0.0f,  1.0f, 0.0f,  -1.7071f,
+		-0.5f, 0.5f,  0.5f,  1.0f, 1.0f,   1.7071f,
+		 0.5f, 0.5f,  0.5f,  1.0f, 0.0f,   1.7071f,
+		 0.0f, 1.5f,  0.0f,  0.0f, 0.0f,   1.7071f,
+		-0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		-0.5f, 0.5f,  0.5f,  0.0f, 1.0f,   1.7071f,
+		 0.0f, 1.5f,  0.0f,  1.0f, 0.0f,  -1.7071f,
+		 0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		 0.5f, 0.5f,  0.5f,  1.0f, 0.0f,   1.7071f,
+		 0.0f, 1.5f,  0.0f,  0.0f, 0.0f,   1.7071f,
 		 // Вторая пирамида
 		// Основание
-		-0.5f, 0.0f, -0.5f,   0.5f, -1.5f,  0.5f,
-		 0.5f, 0.0f, -0.5f,   0.5f, -1.5f,  0.5f,
-		 0.5f, 0.0f,  0.5f,   0.5f, -1.5f,  0.5f,
-		-0.5f, 0.0f, -0.5f,   0.5f, -1.5f,  0.5f,
-		 0.5f, 0.0f,  0.5f,   0.5f, -1.5f,  0.5f,
-		-0.5f, 0.0f,  0.5f,   0.5f, -1.5f,  0.5f,
+		-0.5f, 0.5f, -0.5f,   0.5f, -1.5f,  0.5f,
+		 0.5f, 0.5f, -0.5f,   0.5f, -1.5f,  0.5f,
+		 0.5f, 0.5f,  0.5f,   0.5f, -1.5f,  0.5f,
+		-0.5f, 0.5f, -0.5f,   0.5f, -1.5f,  0.5f,
+		 0.5f, 0.5f,  0.5f,   0.5f, -1.5f,  0.5f,
+		-0.5f, 0.5f,  0.5f,   0.5f, -1.5f,  0.5f,
 		// Боковые грани
-		-0.5f, 0.0f, -0.5f,  -0.5f, 0.7071f, -1.7071f,
-		 0.5f, 0.0f, -0.5f,   1.5f, 0.7071f, -1.7071f,
-		 0.0f,-1.0f,  0.0f,   1.5f, 2.7071f, -1.7071f,
-		-0.5f, 0.0f,  0.5f,  -1.5f, 2.7071f,  1.7071f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 2.7071f,  1.7071f,
-		 0.0f,-1.0f,  0.0f,   0.5f, 0.7071f,  1.7071f,
-		-0.5f, 0.0f, -0.5f,  -0.5f, 0.7071f, -1.7071f,
-		-0.5f, 0.0f,  0.5f,  -1.5f, 0.7071f,  1.7071f,
-		 0.0f,-1.0f,  0.0f,  -1.5f, 2.7071f, 1.7071f,
-		 0.5f, 0.0f, -0.5f,   1.5f, 2.7071f, -1.7071f,
-		 0.5f, 0.0f,  0.5f,   0.5f, 2.7071f,  1.7071f,
-		 0.0f, -1.0f,  0.0f,  0.5f, 0.7071f,  1.7071f
+		-0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		 0.5f, 0.5f, -0.5f,  0.0f, 1.0f,  -1.7071f,
+		 0.0f,-0.5f,  0.0f,  1.0f, 0.0f,  -1.7071f,
+		-0.5f, 0.5f,  0.5f,  1.0f, 1.0f,   1.7071f,
+		 0.5f, 0.5f,  0.5f,  1.0f, 0.0f,   1.7071f,
+		 0.0f,-0.5f,  0.0f,  0.0f, 0.0f,   1.7071f,
+		-0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		-0.5f, 0.5f,  0.5f,  0.0f, 1.0f,   1.7071f,
+		 0.0f,-0.5f,  0.0f,  1.0f, 0.0f,   1.7071f,
+		 0.5f, 0.5f, -0.5f,  1.0f, 1.0f,  -1.7071f,
+		 0.5f, 0.5f,  0.5f,  1.0f, 0.0f,   1.7071f,
+		 0.0f, -0.5f,  0.0f, 0.0f, 0.0f,   1.7071f,
 	};
 
 
-
+	//------------------------------------------------------1----------------------------------------------------------
 	// 1. Настраиваем VAO (и VBO) куба
 	unsigned int VBO, cubeVAO;
 	glGenVertexArrays(1, &cubeVAO);
@@ -169,7 +237,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nChannels;
-	unsigned char* data = SOIL_load_image("rainbow_texture.png", &width, &height, &nChannels, SOIL_LOAD_RGB);
+	unsigned char* data = SOIL_load_image("CrystalTexture.png", &width, &height, &nChannels, SOIL_LOAD_RGB);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -181,6 +249,51 @@ int main()
 		std::cout << "texture error\n";
 	}
 	SOIL_free_image_data(data);
+
+	//--------------------------------------------------2--------------------------------------------------------------
+	
+	// 3. Настраиваем VAO (и VBO) второй подставки для фигуры
+	unsigned int secondObjectVBO, secondObjectVAO;
+	glGenVertexArrays(1, &secondObjectVAO);
+	glGenBuffers(1, &secondObjectVBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, secondObjectVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondObjectVertices), secondObjectVertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(secondObjectVAO);
+
+	// Координатные атрибуты
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Атрибуты нормалей
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//texture for second object
+	unsigned int secondObjectTexture;
+	glGenTextures(1, &secondObjectTexture);
+	glBindTexture(GL_TEXTURE_2D, secondObjectTexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	unsigned char* secondObjectData = SOIL_load_image("StandTexture.png", &width, &height, &nChannels, SOIL_LOAD_RGB);
+	if (secondObjectData)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, secondObjectData);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Second object texture error\n";
+	}
+	SOIL_free_image_data(secondObjectData);
+	
+	//----------------------------------------------------------------------------------------------------------------
 
 	// Цикл рендеринга
 	float angle = 50.0f;
@@ -197,6 +310,7 @@ int main()
 		// Рендеринг
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//----------------------------------------------------------------------------------------------------------------
 		glBindTexture(GL_TEXTURE_2D, texture);
 		// Убеждаемся, что активировали шейдер прежде, чем настраивать uniform-переменные/объекты_рисования
 		lightingShader.use();
@@ -210,14 +324,14 @@ int main()
 		lightingShader.setInt("texture_diffuse1", 0);
 
 		// Преобразования Вида/Проекции
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 0.4f / 0.3f, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom+50), 0.4f / 0.3f, 1.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 		lightingShader.setMat4("projection", projection);
 		lightingShader.setMat4("view", view);
 
 		// Мировое преобразование
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.2f, 1.0f, 0.0f));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.1f, -0.8f, 0.0f));
 		lightingShader.setMat4("model", model);
 
 		// Рендеринг куба
@@ -235,6 +349,32 @@ int main()
 
 		glBindVertexArray(lightVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//----------------------------------------------------------------------------------------------------------------
+		lightingShader.setVec3("lightColor", 0.2f, 0.2f, 0.2f);
+		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setInt("texture_diffuse1", 0);
+
+		glBindTexture(GL_TEXTURE_2D, secondObjectTexture);
+		lightingShader.use();
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
+		glm::mat4 secondObjectModel = glm::mat4(1.0f);
+		lightingShader.setMat4("model", secondObjectModel);
+
+		glBindVertexArray(secondObjectVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 48);
+
+		// Также отрисовываем наш объект-"лампочку" 
+		lampShader.use();
+		lampShader.setMat4("projection", projection);
+		lampShader.setMat4("view", view);
+		secondObjectModel = glm::mat4(1.0f);
+		secondObjectModel = glm::translate(secondObjectModel, lightPos);
+		secondObjectModel = glm::scale(secondObjectModel, glm::vec3(0.2f));
+		lampShader.setMat4("model", secondObjectModel);
+
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 48);
 
 
 		// glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
